491f6e6d9523e700419b0a1e8f7f2e1f58dbcb05
Brstone2002 <105527477+Brstone2002@users.noreply.github.com>
2024-04-01 22:42:31 -0500

    finished exercises 4-7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear;
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        isLeapYear = true;
+    } else {
+        isLeapYear = false;
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let total = (tempF - 32) * 5 / 9;
+  total = total.toFixed(1);
+  return parseFloat(total);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let total = tempC * 9 / 5 + 32;
+  total = total.toFixed(1);
+  return parseFloat(total);
 };
 
 // Do not edit below this line
